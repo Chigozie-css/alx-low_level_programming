3b8d82027588705188ec8d315492d8c4bb8dddbf
Chigozie-css <chigoziedivine62@example.com>
2023-09-07 11:51:15 -0700

    Fifth

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/** 
- * main - entry point
+/**
+ * main - Entry point
  *
- * Description: A program that prints with put function
+ * Description: A C program that prints with put function
  *
- * Return: Always return 0 (success)
- */
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
 {
