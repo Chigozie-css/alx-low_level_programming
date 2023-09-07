88d2eaaa3d2fc1c2405202793a554ba2ad805616
Chigozie-css <chigoziedivine62@example.com>
2023-09-07 12:34:47 -0700

    Task seven

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,8 +10,6 @@
 
 int main(void)
 {
-
-	/* sizeof evaluates the size of a variable */
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of an double: %lu byte(s)\n", sizeof(double));
@@ -20,5 +18,4 @@ int main(void)
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
 	return (0);
-
 }
