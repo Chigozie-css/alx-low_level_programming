32ff6073fbc4152a8cd0d480c0a0203743964152
Chigozie-css <chigoziedivine62@example.com>
2023-09-08 13:44:21 -0700

    Task four

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,33 +1,21 @@
 #include <stdio.h>
 
-
 /**
  * main - Entry point
  *
  * Description: prints the alphabet in lowercase, and then in uppercase, followed by a new line
- *
  * Return: Always O (Success)
 */
-
 int main(void)
-
 {
-
 char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
         int i;
 
-
         for (i = 0; i < 52; i++)
-
         {
-
                 putchar(alp[i]);
-
         }
-
         putchar('\n');
-
         return (0);
-
 }
