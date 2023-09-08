f3ed3f0397b6a84a1e964eef3174c31ef64bfc27
Chigozie-css <chigoziedivine62@example.com>
2023-09-08 11:31:35 -0700

    Task one

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,19 +1,15 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
 
 
 /**
-
- * main - Determines if a number is positive, negative or zero.
-
- *
-
- * Return: Always 0 (Success)
-
- */
+* main - Determines if a number is positive, negative or zero.
+*
+* Description: print value of n satus; zero, positive or negative
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 
