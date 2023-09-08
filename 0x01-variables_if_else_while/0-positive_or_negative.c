be82c03ee6b7de344c416f30bc359a7c94687bda
Chigozie-css <chigoziedivine62@example.com>
2023-09-08 10:20:17 -0700

    Task one

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,21 +7,27 @@
  *
  * description: value of n status; positive, negative and zero
  *
- * reture: aways 0
+ * reture: aways 0 (success)
  */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	/*Code*/
 	if(n > 0)
+	{
 		printf("%i is positive \n", n);
+	}
 	else if(n == 0)
+	{
 		printf("%i is zero \n", n);
+	}
 	else
+	{
 		printf("%i is negative \n", n);
+	}
 	return 0;
 }
