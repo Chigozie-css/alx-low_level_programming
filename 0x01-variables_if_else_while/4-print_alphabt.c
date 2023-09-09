def306ea3a15e852ff6dc98dfff1ab7fbd8a9e63
Chigozie-css <chigoziedivine62@example.com>
2023-09-09 05:09:18 -0700

    Task five

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print alphabets without e and q
+ *
+ * Return: 0 (idf successful)
+ */
+
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		if (alpha == 'e' || alpha == 'q')
+			++alpha;
+		putcha(alpha);
+		++alpha;
+	}
+	putchar('\n');
+
+	return (0);
+}
