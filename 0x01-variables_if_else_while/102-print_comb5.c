cb802a2dacc85e7a87e3394d551a75a222e71248
Chigozie-css <chigoziedivine62@example.com>
2023-09-09 08:47:29 -0700

    Last task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,35 +1,39 @@
 #include	<stdio.h>
 
 /**
-	*	main	-	Starting	point
+	*	main	-	Entry	point
 	*
 	*	Description:	Prints	all	possible	combinations	of	two	two-digit	numbers
 	*
-	*	Return:	Return	0	if	successful
+	*	Return:	0	if	successful
 	*/
 
 int	main(void)
 {
-	int	num1,	num2;
+	int	num1	=	0,	num2;
 
-	for	(num1	=	0;	num1	<	100;	num1++)
+	while	(num1	<=	99)
 	{
-		for	(num2	=	0;	num2	<	100;	num2++)
+		num2	=	num1;
+		while	(num2	<=	99)
 		{
-			if	(num1	<	num2)
+			if	(num2	!=	num1)
 			{
 				putchar((num1	/	10)	+	48);
 				putchar((num1	%	10)	+	48);
 				putchar('	');
 				putchar((num2	/	10)	+	48);
 																																putchar((num2	%	10)	+	48);
+
 				if	(num1	!=	98	||	num2	!=	99)
 				{
 					putchar(',');
 					putchar('	');
 				}
 			}
+			++num2;
 		}
+		++num1;
 	}
 	putchar('\n');
 
