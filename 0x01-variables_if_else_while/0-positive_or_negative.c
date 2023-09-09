e3ad12e4a6339fab3d2b85ffab273faf1c79480b
Chigozie-css <chigoziedivine62@example.com>
2023-09-09 04:11:24 -0700

    Task one

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,35 +1,28 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-
 
 /**
-* main - Determines if a number is positive, negative or zero.
-*
-* Description: print value of n satus; zero, positive or negative
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Description: value of n status, positive, zero and negative
+ *
+ * return - Always 0 (successful)
+ */
 
 int main(void)
 {
-        int n;
+	int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-        if (n > 0)
-        {
-                printf("%d is positive\n", n);
-        }
-        else if (n == 0)
-        {
-                printf("%d is zero\n", n);
-        }
-        else
-        {
-                printf("%d is negative\n", n);
-        }
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n == 0)
+		printf("%i is zero\n", n);
+	else
+		printf("%i is negative\n", n);
 
-        return (0);
+	return (0);
 }
