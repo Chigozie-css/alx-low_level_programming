a6f195905a4753d62a6fb8f75de06afc65457170
Chigozie-css <chigoziedivine62@gmail.com>
2023-09-12 17:11:49 -0700

    Task ten

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - Print the 9 table
+ *
+ * Example  0,0,0,0,...
+ * 0,1,2,3,...
+ */
+
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; ++i)
+	{
+		_putchar(48);
+		for (j = 1; j <= 9; ++j)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			k = i * j;
+
+			if (k <= 9)
+				_putchar(' ');
+			else
+				_putchar((k / 10) + 48);
+			_putchar((k / 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
