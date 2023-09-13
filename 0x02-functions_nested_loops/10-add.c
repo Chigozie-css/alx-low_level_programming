5319d83017b600804f43a84afa9c2cc699828cba
Chigozie-css <chigoziedivine62@gmail.com>
2023-09-12 17:40:23 -0700

    Task eleven

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - addition of two integers
+ *
+ * @s: first input
+ * @t: second input
+ *
+ * Return: s and t
+ */
+
+int add(int s, int t)
+{
+	int sum = s + t;
+
+	return (sum);
+}
