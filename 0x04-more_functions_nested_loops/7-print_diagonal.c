6fce7553b3e6b8dd7fb7e3d8da1e25108a8dd2aa
Chigozie-css <chigoziedivine62@gmail.com>
2023-09-14 12:28:50 -0700

    Task 8

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - a function that draws a diagonal line
+ *
+ * @n:  is the number of times the character \ should be printed
+ */
+
+void print_diagonal(int n)
+{
+	int set, gap;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (set = 0; set <= n; ++set)
+		{
+			for (gap = 1; gap <= set; ++gap)
+				_putchar(' ');
+			_putchar(98);
+			_putchar('\n');
+		}
+	}
+}
