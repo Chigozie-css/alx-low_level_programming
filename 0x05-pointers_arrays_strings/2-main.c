f4b3ab3b70ab1404ff7e2b0268231294507cad8d
Chigozie <80043928+Chigozie-css@users.noreply.github.com>
2023-09-18 20:41:33 +0100

    Update 2-main.c

--- 0x05-pointers_arrays_strings/2-main.c
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -1,19 +1,1 @@
-#include "main.h"
-#include <stdio.h>
 
-/**
- * main - check code
- *
- * Return: Always 0
- */
-
-int main(void)
-{
-	char *str;
-	int len;
-
-	str = "My first strglen!";
-	len = _strlen(str);
-	printf("%d\n", len);
-	return (0);
-}
