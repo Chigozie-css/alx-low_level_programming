8d2650ad6ee3c9ce8beadfe5518564e459f5c836
Chigozie-css <chigoziedivine62@gmail.com>
2023-09-28 13:32:38 -0700

    A  function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,30 +1,6 @@
 #include "main.h"
 
 /**
- * checker - Compares if identical
- *
- * @s1: Address one
- * @s2: Address two
- * @i: Index
- * @j: Special index
- *
- * Return: 1 if palindrome, 0 otherwise
- */
-
-int checker(char *s1, char *s2, int i, int j)
-{
-	if (s1[i] == '\0' && s2[j] == '\0')
-		return (0);
-	if (s1[i] == s2[j])
-		return (checker(s1, s2, i + 1, j + 1));
-	if (s1[i] == '\0' && s2[j] == '*')
-		return (checker(s1, s2, i, j + 1));
-	if (s2[j] == '*')
-		return (checker(s1, s2, i + 1, j) || checker(s1, s2, i, j + 1));
-	return (0);
-}
-
-/**
  * wildcmp - Compares two strings and returns 1 and so on
  *
  * @s1: Address for string
@@ -35,5 +11,13 @@ int checker(char *s1, char *s2, int i, int j)
 
 int wildcmp(char *s1, char *s2)
 {
-	return (checker(s1, s2, 0, 0));
+	if (*s2 == '\0' && *s1 == '\0')
+		return (1);
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	return (0);
 }
