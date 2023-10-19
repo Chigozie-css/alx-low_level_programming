1257873fa5e8e853d1bd8fa73f267546b784d5ef
Chigozie-css <chigoziedivine62@gmail.com>
2023-10-19 10:58:48 -0700

    A function that prints You're beat! and yet, you must allow etc

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,8 @@
-#include <stdio.h>
+#include "lists.h"
 
 /**
  * fun_func - function that prints You're beat! and yet, you must
- * allow,\nI bore my house upon my back\n.
- */
-
-void fun_func(void) __attribute__ ((constructor));
-
-/**
- * fun_func - executes
+ * allow,\nI bore my house upon my back.
  */
 
 void fun_func(void)
