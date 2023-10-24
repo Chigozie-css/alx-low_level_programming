6093cc0d3d91cb57ecf38fed91d7a79985dfa8e3
Chigozie-css <chigoziedivine62@gmail.com>
2023-10-24 11:30:50 -0700

    Task 13 and 14

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,2 +1,39 @@
 #include "lists.h"
 
+/**
+ * free_listint_safe -  frees a listint_t list.
+ *
+ * @h: pointer to node
+ *
+ * Return:  number of elements in the freed list
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	size_t al = 0;
+	int bf;
+	listint_t *cp;
+
+	if (!h || !*h)
+		return (0);
+	while (*h)
+	{
+		bf = *h - (*h)->next;
+		if (bf > 0)
+		{
+			cp = (*h)->next;
+			free(*h);
+			*h = cp;
+			al++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			al++;
+			break;
+		}
+	}
+	*h = NULL;
+	return (al);
+}
