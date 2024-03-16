e48d4bcbe6189280ea38ac32b9169fe14445b7d7
Chigozie-css <chigoziedivine62@gmail.com>
2024-03-16 09:48:55 -0700

    Keygen issue

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -10,32 +10,32 @@
  * @argv: array of arguments
  * Return: 0 on success, 1 on failure
  */
-
 int main(int argc, char **argv)
 {
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: %s <username>\n", argv[0]);
-		return (EXIT_FAILURE);
-	}
+        if (argc != 2)
+        {
+                fprintf(stderr, "Usage: %s <username>\n", argv[0]);
+                return (EXIT_FAILURE);
+        }
 
-	char *username = argv[1];
-	int len = strlen(username);
+        char *username = argv[1];
+        int len = strlen(username);
 
-	srand(time(NULL));
+        srand(time(NULL));
 
-	char key[7];
+        char key[7];
 
-	key[0] = len ^ 59;
-	key[1] = (len * 3) ^ 79;
-	key[2] = (len * 5) ^ 85;
-	key[3] = find_largest_char(username, len);
-	key[4] = multiply_chars(username, len);
-	key[5] = generate_random_char(username);
+        key[0] = (len ^ 59) & 63;
+        key[1] = ((len * 3) ^ 79) & 63;
+        key[2] = ((len * 5) ^ 85) & 63;
+        key[3] = find_largest_char(username, len);
+        key[4] = multiply_chars(username, len);
+        key[5] = generate_random_char(username);
+        key[6] = '\0';
 
-	printf("%s\n", key);
+        printf("%s\n", key);
 
-	return (EXIT_SUCCESS);
+        return (EXIT_SUCCESS);
 }
 
 /**
@@ -45,21 +45,20 @@ int main(int argc, char **argv)
  * @len: length of username
  * Return: largest character found
  */
-
 int find_largest_char(char *usrn, int len)
 {
-	int ch = *usrn;
-	int vch = 0;
+        int ch = *usrn;
+        int vch = 0;
 
-	while (vch < len)
-	{
-		if (ch < usrn[vch])
-			ch = usrn[vch];
-		vch++;
-	}
+        while (vch < len)
+        {
+                if (ch < usrn[vch])
+                        ch = usrn[vch];
+                vch++;
+        }
 
-	srand(ch ^ 14);
-	return ((rand() & 63));
+        srand(ch ^ 14);
+        return ((rand() & 63));
 }
 
 /**
@@ -69,19 +68,18 @@ int find_largest_char(char *usrn, int len)
  * @len: length of username
  * Return: multiplied character
  */
-
 int multiply_chars(char *usrn, int len)
 {
-	int ch = 0;
-	int vch = 0;
+        int ch = 0;
+        int vch = 0;
 
-	while (vch < len)
-	{
-		ch = ch + usrn[vch] * usrn[vch];
-		vch++;
-	}
+        while (vch < len)
+        {
+                ch = ch + usrn[vch] * usrn[vch];
+                vch++;
+        }
 
-	return (((unsigned int)ch ^ 239) & 63);
+        return (((unsigned int)ch ^ 239) & 63);
 }
 
 /**
@@ -90,17 +88,16 @@ int multiply_chars(char *usrn, int len)
  * @usrn: username
  * Return: random character
  */
-
 int generate_random_char(char *usrn)
 {
-	int ch = 0;
-	int vch = 0;
+        int ch = 0;
+        int vch = 0;
 
-	while (vch < *usrn)
-	{
-		ch = rand();
-		vch++;
-	}
+        while (vch < *usrn)
+        {
+                ch = rand();
+                vch++;
+        }
 
-	return (((unsigned int)ch ^ 229) & 63);
+        return (((unsigned int)ch ^ 229) & 63);
 }
