81786cba446b19cef02d2a3273f2cf49cc4209ae
Chigozie-css <chigoziedivine62@gmail.com>
2024-03-16 08:45:53 -0700

    Fixing some issues

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i;
 
 	if (idx == 0)
-		return add_dnodeint(h, n);
+		return (add_dnodeint(h, n));
 
 	current = *h;
 	for (i = 0; current != NULL && i < idx - 1; i++)
@@ -27,7 +27,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node->n = n;
 	new_node->prev = current;
@@ -38,5 +38,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	current->next = new_node;
 
-	return new_node;
+	return (new_node);
 }
--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -14,7 +14,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i;
 
 	if (current == NULL)
-		return -1;
+		return (-1);
 
 	while (current->prev != NULL)
 		current = current->prev;
@@ -23,7 +23,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		current = current->next;
 
 	if (current == NULL)
-		return -1;
+		return (-1);
 
 	if (current->prev != NULL)
 	current->prev->next = current->next;
@@ -34,5 +34,5 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		current->next->prev = current->prev;
 
 	free(current);
-	return 1;
+	return (1);
 }
