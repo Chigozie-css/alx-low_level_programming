24db4202e613800c2b20d7c7e9c9aa5f04462ee8
Chigozie-css <chigoziedivine62@gmail.com>
2024-05-08 06:38:12 -0700

    Task 8

--- 0x1E-search_algorithms/102-interpolation.cc
+++ 0x1E-search_algorithms/102-interpolation.cc
@@ -14,17 +14,18 @@ int interpolation_search(int *array, size_t size, int value)
 	size_t pos, low, high;
 	double f;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	low = 0;
 	high = size - 1;
 
-	while (size)
+	while (low <= high && value >= array[low] && value <= array[high])
 	{
 		f = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
-		pos = (size_t)(low + f);
-		printf("Value checked array[%lu]", pos);
+		pos = low + (size_t)f;
+
+		printf("Value checked array[%d]", (int)pos);
 
 		if (pos >= size)
 		{
@@ -43,9 +44,6 @@ int interpolation_search(int *array, size_t size, int value)
 			low = pos + 1;
 		else
 			high = pos - 1;
-
-		if (low == high)
-			break;
 	}
 
 	return (-1);
